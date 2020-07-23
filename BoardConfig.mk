3fd30f765fa9d44d1cbb82de6b789e47d74028dc
hacker1024 <hacker1024@users.sourceforge.net>
2020-07-23 06:00:29 +0000

    raphael: halium: clean up unneccesary files
    Change-Id: I23ce9eeb57cc0c4510a0cd102366b91bcf0b4c6e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=tty0 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += apparmor=0
+#BOARD_KERNEL_CMDLINE += apparmor=0
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -161,3 +161,6 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/raphael/BoardConfigVendor.mk
+
+# Ubuntu Touch
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(DEVICE_PATH)/charger/images
--- device.mk
+++ device.mk
@@ -19,14 +19,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-#    $(LOCAL_PATH)/overlay-lineage
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
-
-# Device uses high-density artwork where available
-#PRODUCT_AAPT_CONFIG := normal
-#PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -36,52 +31,37 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
-# Camera
-#PRODUCT_PACKAGES += \
-#    Snap
-
 # Camera motor
-#PRODUCT_PACKAGES += \
-#    libdng_sdk.vendor \
-#    libgui_vendor \
-#    vendor.lineage.camera.motor@1.0-service.xiaomi_raphael
-
-# Device-specific settings
-#PRODUCT_PACKAGES += \
-#    XiaomiParts
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    vendor.lineage.camera.motor@1.0-service.xiaomi_raphael
 
 # Display
 PRODUCT_PACKAGES += \
     libvulkan
 
 # Fingerprint
-#PRODUCT_COPY_FILES += \
-#    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 #PRODUCT_PACKAGES += \
 #    lineage.biometrics.fingerprint.inscreen@1.0-service.raphael
 
 # FM
-#PRODUCT_PACKAGES += \
-#    FM2 \
-#    libqcomfm_jni \
-#    qcom.fmradio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # HIDL
-#PRODUCT_PACKAGES += \
-#    android.hidl.base@1.0 \
-#    android.hidl.manager@1.0
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
 
 # HotwordEnrollement
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
-
-# IFAA manager
-#PRODUCT_PACKAGES += \
-#    org.ifaa.android.manager
-
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
 # Init
 PRODUCT_PACKAGES += \
@@ -93,10 +73,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
 
-# Livedisplay
-#PRODUCT_PACKAGES += \
-#    lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
@@ -106,35 +82,45 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # NFC
-#PRODUCT_PACKAGES += \
-#    com.android.nfc_extras \
-#    com.gsma.services.nfc \
-#    com.nxp.nfc.nq \
-#    libnqnfc-nci \
-#    nqnfcee_access.xml \
-#    nqnfcse_access.xml \
-#    NQNfcNci \
-#    SecureElement \
-#    Tag
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    libnqnfc-nci \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
+    NQNfcNci \
+    SecureElement \
+    Tag
 
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service
 
-# Telephony
-#PRODUCT_PACKAGES += \
-#    telephony-ext
-
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
-
-# TextClassifier
-#PRODUCT_PACKAGES += \
-#    textclassifier.bundle1
-
 # WiFi Display
-#PRODUCT_PACKAGES += \
-#    libnl
+PRODUCT_PACKAGES += \
+    libnl
 
-#PRODUCT_BOOT_JARS += \
-#    WfdCommon
+# Ubuntu Touch
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
+    $(LOCAL_PATH)/ubuntu/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
+    $(LOCAL_PATH)/ubuntu/setupusb:system/halium/usr/share/usbinit/setupusb \
+    $(LOCAL_PATH)/ubuntu/mtp-state.conf:system/halium/etc/init/mtp-state.conf \
+    $(LOCAL_PATH)/ubuntu/mtp-server.conf:system/halium/usr/share/upstart/sessions/mtp-server.conf \
+    $(LOCAL_PATH)/ubuntu/apparmor.d/local/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server \
+    $(LOCAL_PATH)/ubuntu/apparmor.d/abstractions/base:system/halium/etc/apparmor.d/abstractions/base \
+    $(LOCAL_PATH)/ubuntu/apparmor.d/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
+    $(LOCAL_PATH)/ubuntu/apparmor.d/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android
+
+#    $(LOCAL_PATH)/ubuntu/ofono.override:system/halium/etc/init/ofono.override \
+#    $(LOCAL_PATH)/ubuntu/timekeeper.conf:system/halium/etc/init/timekeeper.conf \
+#    $(LOCAL_PATH)/ubuntu/config-default.xml:system/halium/usr/share/powerd/device_configs/config-default.xml
+
+PRODUCT_PACKAGES += \
+   charger_res_images
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ril.num_slots=2
+#    ro.t-o.quirk.forcesink=sink.primary_output \
+#    ro.t-o.quirk.forcesource=source.primary_input \
